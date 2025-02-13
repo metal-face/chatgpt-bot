4ad3da71840af84a25240363ee2d3686af02da55
metal-face <hughesbryan3000@gmail.com>
2025-02-12 23:05:24 -0500

    port changed

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def run_discord_bot():
 
 
 async def run_fastapi():
-    config = uvicorn.Config(app, host="0.0.0.0", port=8765, log_level="info")
+    config = uvicorn.Config(app, host="0.0.0.0", port=80, log_level="info")
     server = uvicorn.Server(config)
     await server.serve()
 
